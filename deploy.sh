bd03110f55a374aff576af95776ffd0c1733e612
Aruna Nerella <arunanerellamail@gmail.com>
2021-07-28 18:08:49 +0200

    pull fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 echo "Starting to deploy docker image to an app host.."
 DOCKER_IMAGE=container-images
 IMAGE_TAG=202107
-docker pull $DOCKER_IMAGE:$IMAGE_TAG
+docker pull 511572627495.dkr.ecr.us-east-1.amazonaws.com/$DOCKER_IMAGE:$IMAGE_TAG
 docker ps -q --filter ancestor=$DOCKER_IMAGE:$IMAGE_TAG | xargs -r docker stop
 docker run -d -p 8080:8080 $DOCKER_IMAGE:$IMAGE_TAG
